ffa25513e406821824c4d1264cda47f94fe8bb14
doesnoti <41094189+doesnoti@users.noreply.github.com>
2020-03-18 22:25:13 +0300

    Add files via upload

--- Fill/Fill/Fill.cpp
+++ Fill/Fill/Fill.cpp
@@ -15,95 +15,152 @@ void PrintField( vector < vector <int> >& field )
     cout << endl;
 }
 
-int main()                          //Наше поле "цветов"
-{                                   //y 0  1  2  3  4  5  6  7  8        x
-    vector < vector <int> > field = { { 0, 0, 0, 3, 3, 3, 3, 3, 3 },   //0
-                                      { 0, 0, 3, 1, 1, 1, 2, 2, 3 },   //1
-                                      { 0, 3, 2, 2, 2, 1, 1, 3, 0 },   //2 
-                                      { 3, 1, 1, 3, 2, 2, 2, 3, 0 },   //3
-                                      { 3, 1, 3, 3, 2, 2, 3, 0, 0 },   //4
-                                      { 0, 3, 1, 3, 2, 2, 3, 0, 0 },   //5
-                                      { 0, 0, 3, 1, 2, 3, 0, 0, 0 },   //6
-                                      { 0, 3, 1, 2, 2, 3, 0, 0, 0 },   //7
-                                      { 0, 3, 1, 2, 3, 0, 0, 0, 0 },   //8
-                                      { 0, 3, 3, 3, 3, 0, 0, 0, 0 }, };//9
+int main()                          //Наше поле "цветов" ("3" - граница; "1, 2" - другие цвета; "0" - цвет заливки;)
+{                                   //y 0  1  2  3  4  5  6  7  8  9        x
+    vector < vector <int> > field = { { 3, 3, 3, 3, 3, 3, 3, 3, 3, 3 },   //0
+                                      { 3, 2, 1, 3, 2, 2, 1, 3, 1, 3 },   //1
+                                      { 3, 2, 1, 3, 2, 3, 2, 3, 2, 3 },   //2 
+                                      { 3, 1, 2, 3, 1, 3, 0, 3, 1, 3 },   //3
+                                      { 3, 1, 1, 3, 2, 3, 0, 3, 0, 3 },   //4
+                                      { 3, 2, 1, 3, 1, 3, 1, 3, 2, 3 },   //5
+                                      { 3, 2, 2, 3, 1, 3, 2, 3, 2, 3 },   //6
+                                      { 3, 0, 0, 0, 0, 3, 2, 3, 2, 3 },   //7
+                                      { 3, 2, 2, 2, 2, 3, 1, 2, 0, 3 },   //8
+                                      { 3, 3, 3, 3, 3, 3, 3, 3, 3, 3 }, };//9
 
     PrintField(field);
 
     queue<int> qxU, qyU;
 
-    int x = 1, y = 7;               //Начальная точка - задаём любую в пределах контура
+    int x = 8, y = 1;                               //Начальная точка - задаём любую в пределах контура
 
     qxU.push(x);
     qyU.push(y);
 
     int i, j;
-    queue<int> qxD, qyD;                    //Разделяем заливку по движениям вверх "U" и вниз "D"
-    bool flagU = false, flagD = false;      //Флаги для контролирования неровностей контура и избегания лишних действий с очередью
-    
-    while (!qxU.empty())
+    queue<int> qxD, qyD;                            //Разделяем заливку по движениям вверх "U" и вниз "D"
+
+    while (!qxU.empty() || !qxD.empty())
     {
-        i = qxU.front();
-        j = qyU.front();
-        qxU.pop();
-        qyU.pop();
-
-        while (field[i][j - 1] != 3)                    //Всегда сдвигаемся максимально влево
-            --j;
-
-        while (field[i][j] != 3)
+        while (!qxU.empty())                        //Заливка вверх
         {
-            field[i][j] = 0;
+            bool flagU = false, flagD = false,      //Флаги для контролирования неровностей контура и избегания лишних действий с очередью
+                flagExсeptU = false;                //флаг для фрагментов с цветом заливки
+            i = qxU.front();
+            j = qyU.front();
+            qxU.pop();
+            qyU.pop();
 
-            if ((field[i - 1][j] != 3 && field[i - 1][j] != 0) && flagU == false)       //Если сверху нет контура, закрашенного цвета и флаг
-            {                                                                           //говорит, что это не ячейка, которая идёт подряд
-                qxU.push(i - 1);
-                qyU.push(j);
-                flagU == true;
-            }
+            while (field.at(i).at(j - 1) != 3)                          //Всегда сдвигаемся максимально влево
+                --j;
 
-            if (field[i - 1][j] == 3)                       //Если попадается неровность, то меняем флаг
-                flagU == false;
-            
-            if ((field[i + 1][j] != 3 && field[i + 1][j] != 0) && flagD == false)
+            while (field.at(i).at(j) != 3)
             {
-                qxD.push(i + 1);
-                qyD.push(j);
-                flagD == true;
+                field.at(i).at(j) = 0;
+
+                if (field.at(i - 1).at(j) == 0)                         //Если попадается учаток цвета заливки
+                {
+                    int n = -1;
+
+                    while (field.at(i + n).at(j) == 0 && field.at(i + n).at(j) != 3)
+                        --n;
+
+                    if (field.at(i + n).at(j) != 3 && flagExсeptU == false)
+                    {
+                        qxU.push(i + n);
+                        qyU.push(j);
+                        flagExсeptU = true;
+                    }
+
+                    if (field.at(i + n).at(j) == 3)                 //Если попадается неровность, то меняем флаг
+                        flagExсeptU = false;
+                }
+
+                if (field.at(i - 1).at(j) != 3 && flagU == false)   //Если сверху нет контура, закрашенного цвета и флаг
+                {                                                   //говорит, что это не ячейка, которая идёт подряд
+                    qxU.push(i - 1);
+                    qyU.push(j);
+                    flagU = true;
+                }
+
+                if (field.at(i - 1).at(j) == 3)
+                    flagU = false;
+
+                ////////////////////////////////////////////////////////////
+
+                if ((field.at(i + 1).at(j) != 3 && field.at(i + 1).at(j) != 0) && flagD == false)
+                {
+                    qxD.push(i + 1);
+                    qyD.push(j);
+                    flagD = true;
+                }
+
+                if (field.at(i + 1).at(j) == 3)
+                    flagD = false;
+
+                ++j;
             }
-
-            if (field[i + 1][j] == 3)
-                flagD == false;
-
-            ++j;
         }
-    }
 
-    while (!qxD.empty())
-    {
-        i = qxD.front();
-        j = qyD.front();
-        qxD.pop();
-        qyD.pop();
-
-        while (field[i][j - 1] != 3)
-            --j;
-
-        while (field[i][j] != 3)
+        while (!qxD.empty())                //Заливка вниз
         {
-            field[i][j] = 0;
+            bool flagU = false, flagD = false,
+                flagExсeptD = false;
 
-            if ((field[i + 1][j] != 3 && field[i + 1][j] != 0) && flagD == false)
+            i = qxD.front();
+            j = qyD.front();
+            qxD.pop();
+            qyD.pop();
+
+            while (field.at(i).at(j - 1) != 3)
+                --j;
+
+            while (field.at(i).at(j) != 3)
             {
-                qxD.push(i + 1);
-                qyD.push(j);
-                flagD == true;
+                field.at(i).at(j) = 0;
+
+                if ((field.at(i - 1).at(j) != 3 && field.at(i - 1).at(j) != 0) && flagU == false)
+                {
+                    qxU.push(i - 1);
+                    qyU.push(j);
+                    flagU = true;
+                }
+
+                if (field.at(i - 1).at(j) == 3)
+                    flagU = false;
+
+                ///////////////////////////////////////////////////
+
+                if (field.at(i + 1).at(j) == 0)
+                {
+                    int n = 1;
+
+                    while (field.at(i + n).at(j) == 0 && field.at(i + n).at(j) != 3)
+                        ++n;
+
+                    if (field.at(i + n).at(j) != 3 && flagExсeptD == false)
+                    {
+                        qxD.push(i + n);
+                        qyD.push(j);
+                        flagExсeptD = true;
+                    }
+
+                    if (field.at(i + n).at(j) == 3)
+                        flagExсeptD = false;
+                }
+
+                if (field.at(i + 1).at(j) != 3 && flagD == false)
+                {
+                    qxD.push(i + 1);
+                    qyD.push(j);
+                    flagD = true;
+                }
+
+                if (field.at(i + 1).at(j) == 3)
+                    flagD = false;
+
+                ++j;
             }
-
-            if (field[i + 1][j] == 3)
-                flagD == false;
-
-            ++j;
         }
     }
 
